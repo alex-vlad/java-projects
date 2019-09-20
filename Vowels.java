cd39ed2424bd411c7c3f33ab39f23577112ff62a
Alex Vlad <55590205+alex-vlad@users.noreply.github.com>
2019-09-21 01:43:53 +0300

    Update Vowels.java

--- Vowels.java
+++ Vowels.java
@@ -1,4 +1,3 @@
-// Enter a string and the program counts the number of vowels in the text. For added complexity have it report a sum of each vowel found.
 public class Vowels {
 	public static void main(String[] args) {
 	String str="jAva is coOl";
