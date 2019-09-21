f5670a59c957ff3d39ae91ac16994b8367d777b8
Alex Vlad <55590205+alex-vlad@users.noreply.github.com>
2019-09-21 17:00:55 +0300

    Update BinDec.java

--- bintodec/src/bintodec/BinDec.java
+++ bintodec/src/bintodec/BinDec.java
@@ -9,43 +9,41 @@ public class BinDec {
 		String answer = null;
 		do
 		{
-			System.out.print("1.Convert Decimal to Binary\n2.Convert Binary to Decimal\n" +
-				"Enter an option : ");
+			System.out.print("1.Convert Decimal to Binary\n2.Convert Binary to Decimal\n");
 			int choice = in.nextInt();
 			if(choice == 1)
 			{
 				System.out.print("Enter the decimal value: ");
-				int num = in.nextInt();
-				System.out.println("The number in binary is " + decToBin(num));
+				int n = in.nextInt();
+				System.out.println("The number in binary is " + decToBin(n));
 			}
 			else if(choice == 2)
 			{
 				System.out.print("Enter the binary value: ");
-				String num = in.next();
+				String num= in.next();
 				System.out.println("The number in decimal is " + binToDec(num));
 			}
 			else
 			{
-				System.out.println("Wrong input entered");
+				System.out.println("Wrong input");
 			}
-			System.out.print("Do you want to try again?(y/n) : ");
+			System.out.print("Do you want to try again?(y/n): ");
 			answer = in.next();
 		}while(answer.equalsIgnoreCase("y"));
 		in.close();
 	}
 
-	private static long binToDec(String num) {
-		long dec = 0;
+	private static double binToDec(String num) {
+		double dec = 0;
 		for(int i=num.length()-1;i>=0;i--)
 		{
-			long power = (long) Math.pow(2.0, num.length()-i-1);
+			double power = (double) Math.pow(2.0, num.length()-i-1);
 			dec+=(power*(num.charAt(i)-'0'));
 		}
 		return dec;
 	}
 
-	private static String decToBin(int num) {
-		
+	private static String decToBin(int num) {	
 		String bin = "";
 		do
 		{
